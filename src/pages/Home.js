16ee3c77388461819b081945bc7a31eed2cc671b
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-11 13:27:23 -0700

    added base homepage structure

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Meredith McCollum | Web Developer</h1>
+        <p>
+        lorem ipsum 
+        </p>
+      </div>
+    );
+  }
