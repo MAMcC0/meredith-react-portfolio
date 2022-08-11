131e49c178c9ff646ee176cf4e0435663f2f0ef4
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-11 13:25:27 -0700

    added basic structure to about page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,1 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+
+export default function About() {
+  return (
+    <div>
+      <h1>About |</h1>
+      <p>
+      lorem ipsum 
+      </p>
+    </div>
+  );
+}
