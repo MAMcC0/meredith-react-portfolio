1646c0a51b5758a03134631cd61fb578b1003515
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-11 13:26:19 -0700

    added basic structure to contact pate"

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact |</h1>
+        <p>
+        lorem ipsum 
+        </p>
+      </div>
+    );
+  }
