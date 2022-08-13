a093121708efa7bc37533e02146fefe55e8bf62e
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-13 00:55:32 -0700

    deploy

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+      <div>
+        <div id="contact-deco">
+        <h1>Contact</h1>
+        <p>
+        lorem ipsum 
+        </p>
+        </div>
+      </div>
+    );
+  }
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default function Portfolio() {
+    return (
+      <div>
+
+        <div id="port-deco">
+        <h1>Portfolio</h1>
+        <p>
+        lorem ipsum 
+        </p>
+        </div>     
+      </div>
+      
+    );
+  }
