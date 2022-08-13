0c11d67ff84cebffd58e30e705cb89eb94a51078
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-13 11:51:57 -0700

    fixed styling from revert

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 export default function Resume() {
-    return (
-      <div>
-        <h1>Resume |</h1>
-        <p>
-        Add downloadable link here
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div id="resume-deco">
+      <h1>Resume</h1>
+      <p><a href={require('../photos/Meredith McCollum Resume.pdf')} download>Download Here</a></p>
+    </div>
+  );
+}
