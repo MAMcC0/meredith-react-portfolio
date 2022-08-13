3ac7f2ddfc5e65be8d5d2509cf3ce17bdccbf094
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-13 08:58:52 -0700

    added links to portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,23 +9,31 @@ export default function Portfolio() {
         <div id="port-deco" >
         <h1>Portfolio</h1>
         <div>
-        <p  id="beg-luck">
+        <p  id="beg-luck"><a href='https://sensationnel-baguette-60188.herokuapp.com/login '>
         Beginners Luck 
+        </a>
         </p>
-        <p id="na-football">
+        <p id="na-football"><a href='https://down-with-the-fitness.herokuapp.com/'>
         Not-American Football
-        </p>
-        <p id="dwt-fitness">
+        </a></p>
+        <p id="dwt-fitness"><a href='https://down-with-the-fitness.herokuapp.com/ '>
         Down With the Fitness
+        </a>
         </p>
-        <p id="scheduler">
+        <p id="scheduler"><a href='https://mamcc0.github.io/day-planner/'>
+
         Scheduler
+        </a>
         </p>
-        <p id="note-taker">
+        <p id="note-taker"><a href='https://vast-tundra-99915.herokuapp.com/'>
         Note Taker
+        </a>
         </p>
-        <p id="weater-app">
+        <p id="weather-app"><a href='https://mamcc0.github.io/weather-dashboard/'>
+
+        
         Weather App
+        </a>
         </p>
         </div>
         </div>     
