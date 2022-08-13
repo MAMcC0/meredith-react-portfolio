d8f0095406253ae67f78571e503ca26f84833f94
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-13 07:31:34 -0700

    for deployment

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function Contact() {
     return (
+      <Form>
+      <Form.Group className="mb-3" controlId="contactEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="contactFirstName">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" placeholder="First Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="contactLastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" placeholder="Last Name" />
+      </Form.Group>
+      <InputGroup>
+        <InputGroup.Text>With textarea</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="Leave a message!" />
+      </InputGroup>
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     )
   }
