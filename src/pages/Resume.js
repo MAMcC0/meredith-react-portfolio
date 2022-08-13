fdbc10091c21b6e5c48021a01045f355b3147f13
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-13 00:57:22 -0700

    deploy with pages

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Resume() {
     return (
-      <div>
+      <div id="resume-deco">
         <h1>Resume |</h1>
         <p>
         Add downloadable link here
